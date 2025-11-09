443fde8b5cb814e3e0a23757ee1504f79270b310
Feyza Yarcı <yarcifeyza@gmail.com>
2025-11-09 17:24:28 +0300

    fix: update MongoDB connection to use full connection string
    - Change initMongoConnection to use MONGODB_URL directly
    - Remove manual connection string construction
    - Fix 'mongodb+srv URI cannot have port number' error
    - Support full MongoDB Atlas connection strings

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 
 export const initMongoConnection = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const password = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
+    const mongoUrl = process.env.MONGODB_URL;
+    
+    if (!mongoUrl) {
+      throw new Error('MONGODB_URL environment variable is not set');
+    }
 
-    const connectionString = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
-
-    await mongoose.connect(connectionString);
+    await mongoose.connect(mongoUrl);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Error while setting up mongo connection', error);
